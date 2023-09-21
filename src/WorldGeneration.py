935451f7cfa5f55b0861fd68e2190ce5c90e8e50
Xanderplayz16 <Xandermckay@icloud.com>
2023-09-21 17:59:58 -0400

    Minor additions and world saving progress

--- src/WorldGeneration.py
+++ src/WorldGeneration.py
@@ -8,7 +8,16 @@ def GenerateWorld(Seed):
     return mcworld
 class World():
     blocks = {}
-    playerpos = (0,0,0)
+    playerpos = [0,0,0]
     hotbar = None
     hotbarcount = None
+    version = 1
+    def Save(self):
+        save = {}
+        save["blocks"] = str(self.blocks)
+        save["playerpos"] = str(self.playerpos)
+    def Load(self, data):
+        self.blocks = data["blocks"]
+        self.playerpos = data["playerpos"]
+
 
